636315a7f7d39defba6bb327bd26026623199ca3
Tiaan Minnaar <tiaanminnaar7@gmail.com>
2024-06-02 12:57:52 +0200

    project euler Q16

--- Question 16/Question 16.cs
+++ Question 16/Question 16.cs
@@ -1,28 +1,54 @@
-﻿int count = 1;
-ulong digitsOf2ToPowerOf1000 = 0;
-ulong sum = 2;
-while (count < 1000)
+﻿string toPowerOf1000 = PowerOf1000("2");
+int sumOfDigitsOfPowerOf1000 = SumOfDigits(toPowerOf1000);
+
+int SumOfDigits(string n)
 {
-    sum *= 2;
-    count++;
-    digitsOf2ToPowerOf1000 = sum;
-}
-
-
-//ulong digitsOf2ToPowerOf1000 = (ulong)MathF.Pow(2, 1000);
-
-int sumOfDigitsOf2ToPowerOf1000 = SumOfDigits(digitsOf2ToPowerOf1000);
-
-int SumOfDigits(ulong n)
-{
-    ulong sum = 0;
-    ulong r;
-
-    while (n > 0)
+    int sum = 0;
+    for (int i = 0; i <= (n.Length - 1); i++)
     {
-        r = n % 10;
-        sum += r;
-        n = n / 10;
+        int m = int.Parse(n[i].ToString());
+        sum += m;
     }
-    return (int)sum;
+    return sum;
 }
+string PowerOf1000(string n)
+{
+    int powerOf = int.Parse(n);
+    int count = 1;
+    string sumStr = string.Empty;
+    while (count <= 999)
+    {
+        string prevSum = string.Empty;
+        int p = 0;
+        string m = Convert.ToString(new string(n.ToString().Reverse().ToArray()));
+        for (int i = 0; i <= (m.Length - 1); i++)
+        {
+            int y = int.Parse(m[i].ToString());
+            int sum = 0;
+            int r;
+            int q = p;
+            p = 0;
+            
+            r = y * powerOf;
+            if (r >= 10)
+            {
+                if (i != (m.Length - 1))
+                {
+                    p = r % 10;
+                    r /= 10;
+                    int o = r;
+                    r = p;
+                    p = o;
+                }
+            }
+            sum += r + q;
+            sumStr = $"{sum}";
+            n = $"{sumStr}{prevSum}";
+            prevSum = n;
+        }
+        count++;
+    }
+    return n;
+}
+
+WriteLine(sumOfDigitsOfPowerOf1000);
